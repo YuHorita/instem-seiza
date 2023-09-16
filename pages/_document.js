c603bd138a02d568cf6f5665684e5cfc9b1d0926
Yu Horita <y.horita1111@gmail.com>
2023-09-16 16:30:09 +0900

    フォント周り修正

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -13,7 +13,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&family=M+PLUS+1p:wght@700&display=swap"
           rel="stylesheet"
-        />
+        /> */}
         <script
           type="text/javascript"
           src="//typesquare.com/3/tsst/script/ja/typesquare.js?64fe9ab4c940489b8184031bac1e02d5&auto_load_font=true"
