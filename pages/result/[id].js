6974179ab434909632c073762ad80504b2463de9
Yu Horita <y.horita1111@gmail.com>
2023-09-19 11:25:52 +0900

    _app.jsに戻してみた

--- pages/result/[id].js
+++ pages/result/[id].js
@@ -152,11 +152,11 @@ export default function Page({ id, designerName, constellationName }) {
           ))}
         </div>
       </section>
-      <Script
+      {/* <Script
         type="text/javascript"
         src="//typesquare.com/3/tsst/script/ja/typesquare.js?64fe9ab4c940489b8184031bac1e02d5"
         charset="utf-8"
-      />
+      /> */}
     </main>
   );
 }
