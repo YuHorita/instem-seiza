908712916d2583cadc53733b49f8b4401f39586d
Yu Horita <y.horita1111@gmail.com>
2023-09-22 17:10:45 +0900

    Update SkySketch.js

--- components/SkySketch.js
+++ components/SkySketch.js
@@ -81,11 +81,10 @@ const Sketch = () => {
       const sketch = new p5((p) => {
         let bg, pg;
 
-        const r = 20,
-          canvasWidth = 1920,
-          canvasHeight = 1080,
-          paddingX = 200,
-          paddingY = 50,
+        const canvasWidth = 3840,
+          canvasHeight = 2160,
+          paddingX = 400,
+          paddingY = 100,
           areaWidth = canvasWidth - paddingX * 2,
           areaHeight = canvasHeight - paddingY * 2,
           itemXMin = designs.sort((a, b) => a.x - b.x)[0].x,
@@ -101,14 +100,13 @@ const Sketch = () => {
           return paddingX + (x - itemXMin) * xRatio;
         }
         function calcY(y) {
-          return paddingY + (y - itemYMin) * yRatio - r / 2;
+          return paddingY + (y - itemYMin) * yRatio;
         }
         function calcRadius(index) {
           const radius =
-            5 +
             ((starCount[index] - starCountMin) /
               (starCountMax - starCountMin)) *
-              45;
+            80;
           return radius;
         }
 
@@ -117,8 +115,8 @@ const Sketch = () => {
             return 0;
           }
           const weight =
-            0.5 +
-            ((lineCount[index1 + "-" + index2] - 1) / (lineCountMax - 1)) * 9.5;
+            1 +
+            ((lineCount[index1 + "-" + index2] - 1) / (lineCountMax - 1)) * 10;
           return weight;
         }
 
@@ -131,6 +129,32 @@ const Sketch = () => {
           canvas.parent(sketchRef.current);
           p.textFont("Gothic MB101 Bold");
           pg = p.createGraphics(p.width, p.height);
+        };
+
+        var sampleLineX = 0,
+          sampleLineY = 0,
+          sampleLineX2 = 0;
+        const easing1 = 0.05;
+        const easing2 = 0.08;
+
+        // designs分のcircleSize, growing, maxSizeを用意
+        let circleSizeArray = [];
+        let growingArray = [];
+        let maxSizeArray = [];
+        for (var i = 0; i < designs.length; i++) {
+          // 円の初期サイズは0~20のランダム
+          circleSizeArray.push(p.random(0, 20));
+          // trueかfalseをランダムに格納
+          growingArray.push(Math.floor(Math.random() * 2) == 0 ? true : false);
+          // calcRadiusの1.5倍の値を最大サイズとする
+          maxSizeArray.push(calcRadius(designs.index) * 1.5);
+        }
+
+        let circleSize = 10; // 円の初期サイズ
+        let maxSize = 20; // 円の最大サイズ
+        let growing = true; // 拡大中かどうかのフラグ
+
+        p.draw = () => {
           p.image(bg, 0, 0, p.width, bg.height * (p.width / bg.width));
           pg.background(37, 39, 50);
           pg.erase();
@@ -140,6 +164,70 @@ const Sketch = () => {
           starLines.forEach((line) => {
             drawLine(line);
           });
+
+          // p.push();
+          // pg.strokeWeight(3);
+          // const dx = 800 - sampleLineX;
+          // let dx2 = 0;
+          // if (dx < 200) {
+          //   dx2 = sampleLineX - sampleLineX2;
+          // }
+          // if (dx < 1 && dx2 < 100) {
+          //   sampleLineX = 0;
+          //   sampleLineX2 = 0;
+          // }
+
+          // const vx = dx * easing1;
+          // const vx2 = dx2 * easing2;
+          // sampleLineX += vx;
+          // sampleLineX2 += vx2;
+          // pg.line(sampleLineX2, 200, sampleLineX, 200);
+
+          // p.pop();
+
+          // ランダムな位置に星を描画
+          // pg.push();
+          // pg.fill(255);
+          // pg.noStroke();
+          // pg.ellipse(
+          //   p.random(0, p.width),
+          //   p.random(0, p.height),
+          //   p.random(10, 50)
+          // );
+          // pg.pop();
+
+          // 拡大縮小のアニメーション
+          if (growing) {
+            circleSize += 0.2; // 拡大
+            if (circleSize >= maxSize) {
+              growing = false; // 最大サイズに達したら縮小へ
+            }
+          } else {
+            circleSize -= 0.2; // 縮小
+            if (circleSize < 1) {
+              growing = true; // 最小サイズに達したら拡大へ
+            }
+          }
+
+          // 上記のコードをdesigns分繰り返す
+          designs.forEach((elm, index) => {
+            if (growingArray[index]) {
+              circleSizeArray[index] +=
+                (maxSize - circleSizeArray[index]) * easing1; // 拡大
+              if (maxSize - circleSizeArray[index] < 1) {
+                growingArray[index] = false; // 最大サイズに達したら縮小へ
+              }
+            } else {
+              circleSizeArray[index] -= circleSizeArray[index] * easing1; // 縮小
+              if (circleSizeArray[index] < 1) {
+                growingArray[index] = true; // 最小サイズに達したら拡大へ
+              }
+            }
+          });
+
+          // 円を描画
+          pg.ellipse(p.width / 2, p.height / 2, circleSize);
+
           pg.noErase();
           p.image(pg, 0, 0);
           designs.forEach((elm) => {
@@ -151,7 +239,11 @@ const Sketch = () => {
           pg.push();
           pg.fill(255);
           pg.noStroke();
-          pg.ellipse(calcX(elm.x), calcY(elm.y), calcRadius(elm.index));
+          pg.ellipse(
+            calcX(elm.x),
+            calcY(elm.y),
+            calcRadius(elm.index) + circleSizeArray[elm.index]
+          );
           pg.pop();
         }
 
